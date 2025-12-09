b2d4fec18d3ec52a4c5b9c5b31011f08b98eed0b
bojitha001 <nawarathnabojitha@gmail.com>
2025-12-09 19:54:00 +0530

    feat: implement LeftSideBar component for enhanced navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,18 @@
+import LeftSideBar from "@/components/navigation/LeftSideBar";
 import NavBar from "@/components/navigation/navbar";
 import React, { ReactNode } from "react";
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
-    <main>
+    <main className="background-light850_dark100 relative">
       <NavBar />
-      {children}
+      <div className="flex">
+        <LeftSideBar />
+
+        <section className="flex-colpx-6 flex min-h-screen flex-1 pt-36 pb-6 max-md:pb-14 sm:px-14">
+          <div className="mx-auto w-full max-w-5xl">{children}</div>
+        </section>
+      </div>
     </main>
   );
 };
