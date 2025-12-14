a317fcaf4d3eaab7480bde58d35a908a84d22e69
bojitha001 <nawarathnabojitha@gmail.com>
2025-12-15 00:04:33 +0530

    feat: implement local search component and add URL query manipulation functions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,22 +1,71 @@
-import { auth, signOut } from "@/auth";
+import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
+import Link from "next/link";
 
-const Home = async () => {
-  const session = await auth();
+const questions = [
+  {
+    _id: "1",
+    title: "how to learn react",
+    description: "I want to learn react, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "Javascript" },
+    ],
+    author: { _id: "1", name: "Jhon Doe" },
+    upvote: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+  {
+    _id: "2",
+    title: "how to learn javascript",
+    description: "I want to learn javascript, can anyone help me?",
+    tags: [
+      { _id: "1", name: "React" },
+      { _id: "2", name: "Javascript" },
+    ],
+    author: { _id: "1", name: "Jhon Doe" },
+    upvote: 10,
+    answers: 5,
+    views: 100,
+    createdAt: new Date(),
+  },
+];
 
-  console.log(session);
+interface SearchParams {
+  searchParams: Promise<{ [key: string]: string }>;
+}
+
+//searchParams are coming from nect js and can use only in the server components
+const Home = async ({ searchParams }: SearchParams) => {
+  const { query = "" } = await searchParams;
+
+  const filteredQuestions = questions.filter((question) =>
+    question.title.toLocaleLowerCase().includes(query?.toLocaleLowerCase())
+  );
+
   return (
     <>
-      <form
-        className="px-10 pt-[100px]"
-        action={async () => {
-          "use server";
-          await signOut({ redirectTo: ROUTES.SIGN_IN });
-        }}
-      >
-        <Button>Log out</Button>
-      </form>
+      <section className="flex w-full flex-col-reverse justify-between gap-4 sm:flex-row sm:items-center">
+        <h1 className="h1-bold text-dark100_light900">All Questions</h1>
+
+        <Button className="primary-gradient !text-light-900 min-h-[46px] px-4 py-3">
+          <Link href={ROUTES.ASK_QUESTION}>Ask a Question</Link>
+        </Button>
+      </section>
+
+      <section className="mt-11">
+        <LocalSearch route="/" imgSrc="/icons/search.svg" placeholder="Search questions..." otherClasses="flex-1" />
+      </section>
+      <section className="mt-11">Home Filter</section>
+
+      <div className="mt-10 flex w-full flex-col gap-6">
+        {filteredQuestions.map((question) => (
+          <h1 key={question._id}>{question.title}</h1>
+        ))}
+      </div>
     </>
   );
 };
---
+++ lib/url.ts
@@ -0,0 +1,40 @@
+import qs from "query-string";
+
+interface UrlQueryParams {
+  params: string;
+  key: string;
+  value: string;
+}
+interface RemoveUrlQueryParams {
+  params: string;
+  keysToRemove: string[];
+}
+
+export const formUrlQuery = ({ params, key, value }: UrlQueryParams) => {
+  const queryString = qs.parse(params);
+
+  queryString[key] = value;
+
+  const url = qs.stringifyUrl({
+    url: window.location.pathname,
+    query: queryString,
+  });
+
+  console.log(url);
+
+  return url;
+};
+
+export const removeKeysFromQuery = ({ params, keysToRemove }: RemoveUrlQueryParams) => {
+  const queryString = qs.parse(params);
+
+  keysToRemove.forEach((key) => delete queryString[key]);
+
+  return qs.stringifyUrl(
+    {
+      url: window.location.pathname,
+      query: queryString,
+    },
+    { skipNull: true }
+  );
+};
