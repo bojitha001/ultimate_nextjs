efe1ecc31316fafa935d9e8355de3052dee94bed
bojitha001 <nawarathnabojitha@gmail.com>
2026-02-22 02:35:00 +0530

    feat: add User model with schema definition for user data

---
+++ database/user.model.ts
@@ -0,0 +1,52 @@
+import { model, models, Schema } from "mongoose";
+
+export interface IUser {
+  name: string;
+  userName: string;
+  email: string;
+  bio?: string;
+  image: string;
+  location?: string;
+  portfolio?: string;
+  reputation?: number;
+}
+
+const UserSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    bio: {
+      type: String,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    location: {
+      type: String,
+    },
+    portfolio: {
+      type: String,
+    },
+    reputation: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true } // automatically adds createdAt and updatedAt fields
+);
+
+const User = models?.user || model<IUser>("User", UserSchema); // checks if model already exists to prevent recompilation errors in development
+
+export default User;
